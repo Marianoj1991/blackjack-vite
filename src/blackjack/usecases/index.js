63c54a0d37b2903a5d8c11c4664400959551ae33
Mariano Jimenez <marianoj.1991@outlook.com>
2023-02-24 11:47:58 -0300

    first commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './take-a-card';
+export { valorCarta } from './card-value';
+export { turnoComputadora } from './turno-computadora';
+export { crearCartaHtml } from './crear-carta-html';
---
+++ src/blackjack/usecases/take-a-card.js
@@ -0,0 +1,16 @@
+
+/**
+ * Esta funcion me permite tomar un carta del deck
+ * @param {Array<String>} deck es un arreglo de string 
+ * @returns {String} retorna una carta
+ */
+
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+    
+}
